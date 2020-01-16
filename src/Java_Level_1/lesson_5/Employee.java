8d26b253186d644d18e7a2da083b01eb35ea21a1
Daniil Gavrilov <dgavrilovdv@gmail.com>
2020-01-16 23:11:39 +0300

    lesson5 completed

--- src/Java_Level_1/lesson_5/Employee.java
+++ src/Java_Level_1/lesson_5/Employee.java
@@ -1,50 +1,68 @@
 package Java_Level_1.lesson_5;
 
 public class Employee {
-    private String name;
-    private String position;
-    private String email;
-    private String phoneNumber;
-    private int salary;
-    private int age;
+  private String name;
+  private String position;
+  private String email;
+  private String phoneNumber;
+  private int salary;
+  private int age;
 
 
-    public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
-        this.name = name;
-        this.position = position;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.salary = salary;
-        this.age = age;
-    }
+  public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
+    this.name = name;
+    this.position = position;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.salary = salary;
+    this.age = age;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getPosition() {
-        return position;
-    }
+  public String getPosition() {
+    return position;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
 
-    public int getSalary() {
-        return salary;
-    }
+  public int getSalary() {
+    return salary;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public void employeeInfo() {
-        System.out.printf("ФИО: %s%nДолжность: %s%nEmail: %s%nPhoneNumber: %s%nSalary: %d%nAge: %d%n",
-                name, position, email, phoneNumber, salary, age);
-    }
+  public void employeeInfo() {
+    System.out.println(toString());
+  }
 
-    }
+/*  @Override // метод сформированный идеей (Alt + insert)
+  public String toString() {
+    return "Employee{" +
+            "name='" + name + '\'' +
+            ", position='" + position + '\'' +
+            ", email='" + email + '\'' +
+            ", phoneNumber='" + phoneNumber + '\'' +
+            ", salary=" + salary +
+            ", age=" + age +
+            '}';
+  }*/
+
+
+@Override
+  public String toString() {
+    return String.format("ФИО: %s%nДолжность: %s%nEmail: %s%nPhoneNumber: %s%nSalary: %d%nAge: %d%n",
+            name, position, email, phoneNumber, salary, age);
+  }
+
+}
--- src/Java_Level_1/lesson_5/MainClass.java
+++ src/Java_Level_1/lesson_5/MainClass.java
@@ -1,32 +1,34 @@
 package Java_Level_1.lesson_5;
 
+
+
 public class MainClass {
 
-    private static Employee[] employees = new Employee[5];
+  private static Employee[] employees = new Employee[5];
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        employees[0] = new Employee("Гаврилов", "проектный менеджер",
-                "dgavrilovdv@gmail.com", "+71", 65000, 30);
-        employees[1] = new Employee("Иванов", "директор",
-                "dgavrilov@gmail.com", "+72", 5000, 31);
-        employees[2] = new Employee("Сидоров", "менеджер",
-                "dgavrilo@gmail.com", "+73", 6500, 32);
-        employees[3] = new Employee("Сергеев", "охранник",
-                "dgavril@gmail.com", "+74", 6000, 40);
-        employees[4] = new Employee("Попов", "секретарь",
-                "dgavri@gmail.com", "+75", 650, 42);
+    employees[0] = new Employee("Гаврилов", "проектный менеджер",
+            "dgavrilovdv@gmail.com", "+71", 65000, 30);
+    employees[1] = new Employee("Иванов", "директор",
+            "dgavrilov@gmail.com", "+72", 5000, 31);
+    employees[2] = new Employee("Сидоров", "менеджер",
+            "dgavrilo@gmail.com", "+73", 6500, 32);
+    employees[3] = new Employee("Сергеев", "охранник",
+            "dgavril@gmail.com", "+74", 6000, 44);
+    employees[4] = new Employee("Попов", "секретарь",
+            "dgavri@gmail.com", "+75", 650, 39);
 
-        for (int i = 0; i < employees.length - 1; i++) {
-            if (employees[i].getAge() > 40) {
-                employees[i].employeeInfo();
-            }
-        }
+    older_40();
+  }
+
+  public static void older_40() {
+    for (Employee tempEmployee : employees) {
+      if (tempEmployee.getAge() > 40) {
+        tempEmployee.employeeInfo();
+      }
     }
-
-    public static void older_40() {
-
-    }
+  }
 
 
 }
